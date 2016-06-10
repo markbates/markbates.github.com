aa56359eb59e4d70677cc1e7ff69bd0807bc2de0
Mark Bates <mark@markbates.com>
2016-06-10 10:58:50 -0400

    updated deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
-
-# Commit changes.
+hugo
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
 then msg="$1"
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master --force
-
-# Come Back
-cd ..
 
 git add -A
 git commit -m "$msg"
-git push origin master
+git push --recurse-submodules=on-demand --force
